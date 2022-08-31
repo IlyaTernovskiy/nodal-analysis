61ba9d02bf764f91b8b56863802b47b92fbe3522
masalimov.tr <mstimur941@gmail.com>
2022-08-31 09:47:59 +0300

    feat: Добавлен endpoint расчета точки пересечения VLP и IPR
    closes: #2

--- src/calculations/nodal.py
+++ src/calculations/nodal.py
@@ -1,4 +1,4 @@
-from shapely.geometry import LineString
+from shapely.geometry import LineString, MultiPoint
 from src.models.models import NodalCalcDecision
 
 def calc_nodal(vlp: dict, ipr: dict):
@@ -22,10 +22,20 @@ def calc_nodal(vlp: dict, ipr: dict):
     vlp_curve = LineString(vlp_points)
     ipr_curve = LineString(ipr_points)
     intersect = vlp_curve.intersection(ipr_curve)
-    intersect_point = NodalCalcDecision(**{
-        "q_liq": intersect.x,
-        "p_wf": intersect.y
-    })
-    nodal_list = [intersect_point]
-    return nodal_list
+    intersect_points = []
+    if isinstance(intersect, MultiPoint):
+        x = [p.x for p in intersect]
+        y = [p.y for p in intersect]
+        for i in range(len(x)):
+            intersect_point = NodalCalcDecision(**{
+                "q_liq": x[i],
+                "p_wf": y[i]
+            })
+            intersect_points.append(intersect_point)
+    else:
+        intersect_points.append(NodalCalcDecision(**{
+            "q_liq": intersect.x,
+            "p_wf": intersect.y
+        }))
+    return intersect_points
 
